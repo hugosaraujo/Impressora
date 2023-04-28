91daede52bc2f031653f490cdf0427d3d6f521ab
Hugo de Araujo <hugo.saraujo@tutanota.com>
2023-04-27 22:41:00 -0300

    Adição de clases abstratas de impressora, e modelo de impressora

---
+++ ImpressoraGenerica/ImpressoraPadrao.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Impressora.Impressora
+{
+    public abstract class ImpressoraPadrao
+    {
+        public abstract void Imprimir();
+    }
+}
---
+++ Modelos/Hp.cs
@@ -0,0 +1,18 @@
+﻿using Impressora.Impressora;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Impressora.Modelos
+{
+    public class Hp : ImpressoraPadrao
+    {
+        public override void Imprimir()
+        {
+            Console.WriteLine("Preparando impressora hp");
+            Console.WriteLine("Imprimindo...");
+        }
+    }
+}
